a3e03e68808a726eaa0c45347d37a17ea493d1ff
Sam Reha <sreha@ucsc.edu>
2017-02-08 11:25:08 -0800

    Buttons now arrange more nicely in build menu

--- UI.js
+++ UI.js
@@ -16,38 +16,58 @@ var Hud = {
 
         // Build Menu UI groups
         var bureauGroup = MainGame.game.make.group();
+        bureauGroup.position.y = MainGame.game.world.height-400; // Magic numbers!
         buildMenu.addChild(bureauGroup);
+
         var merchantGroup = MainGame.game.make.group();
+        merchantGroup.position.y = MainGame.game.world.height-250; // Magic numbers!
         buildMenu.addChild(merchantGroup);
+
         var militaryGroup = MainGame.game.make.group();
+        militaryGroup.position.y = MainGame.game.world.height-100; // Magic numbers!
         buildMenu.addChild(militaryGroup);
 
         // buildMenu -: buyBuildingBtn, seeCoalitionBtn, etc.
-        var buyMansionBtn = MainGame.game.make.button(100, MainGame.game.world.height-100, 'buttonSprite', null, buildMenu, 0, 1, 2, 3);
+        var buyMansionBtn = MainGame.game.make.button(0, 0, 'buttonSprite', null, buildMenu, 0, 1, 2, 3);
         buyMansionBtn.anchor.y = 1;  // Anchor on bottom left corner
-        var mansionText = MainGame.game.make.text(0, -10, "Buy Mansion\n $10", style);
-        mansionText.anchor.set(0);
+        var mansionText = MainGame.game.make.text(0, -40, "Buy Mansion\n $10", style);
+        mansionText.anchor.y = 1;
         buyMansionBtn.addChild(mansionText);
         bureauGroup.addChild(buyMansionBtn);
 
-        var buySuburbBtn = MainGame.game.make.button(230, MainGame.game.world.height-100, 'buttonSprite', null, buildMenu, 0, 1, 2, 3);
+        var buySuburbBtn = MainGame.game.make.button(200, 0, 'buttonSprite', null, buildMenu, 0, 1, 2, 3);
         buySuburbBtn.anchor.y = 1;  // Anchor on bottom left corner
+        var suburbText = MainGame.game.make.text(0, -40, "Buy Suburb\n $10", style);
+        suburbText.anchor.y = 1;
+        buySuburbBtn.addChild(suburbText);
         bureauGroup.addChild(buySuburbBtn);
 
-        var buyApartmentBtn = MainGame.game.make.button(360, MainGame.game.world.height-100, 'buttonSprite', null, buildMenu, 0, 1, 2, 3);
+        var buyApartmentBtn = MainGame.game.make.button(400, 0, 'buttonSprite', null, buildMenu, 0, 1, 2, 3);
         buyApartmentBtn.anchor.y = 1;  // Anchor on bottom left corner
+        var apartmentText = MainGame.game.make.text(0, -40, "Buy Apartment\n $10", style);
+        apartmentText.anchor.y = 1;
+        buyApartmentBtn.addChild(apartmentText);
         bureauGroup.addChild(buyApartmentBtn);
 
-        var buySchoolBtn = MainGame.game.make.button(490, MainGame.game.world.height-100, 'buttonSprite', null, buildMenu, 0, 1, 2, 3);
+        var buySchoolBtn = MainGame.game.make.button(600, 0, 'buttonSprite', null, buildMenu, 0, 1, 2, 3);
         buySchoolBtn.anchor.y = 1;  // Anchor on bottom left corner
+        var schoolText = MainGame.game.make.text(0, -40, "Buy School\n $15", style);
+        schoolText.anchor.y = 1;
+        buySchoolBtn.addChild(schoolText);
         bureauGroup.addChild(buySchoolBtn);
 
-        var buyFactoryBtn = MainGame.game.make.button(620, MainGame.game.world.height-100, 'buttonSprite', null, buildMenu, 0, 1, 2, 3);
+        var buyFactoryBtn = MainGame.game.make.button(0, 0, 'buttonSprite', null, buildMenu, 0, 1, 2, 3);
         buyFactoryBtn.anchor.y = 1;  // Anchor on bottom left corner
+        var factoryText = MainGame.game.make.text(0, -40, "Buy Factory\n $30", style);
+        factoryText.anchor.y = 1;
+        buyFactoryBtn.addChild(factoryText);
         merchantGroup.addChild(buyFactoryBtn);
 
-        var buyArmyBaseBtn = MainGame.game.make.button(750, MainGame.game.world.height-100, 'buttonSprite', null, buildMenu, 0, 1, 2, 3);
+        var buyArmyBaseBtn = MainGame.game.make.button(0, 0, 'buttonSprite', null, buildMenu, 0, 1, 2, 3);
         buyArmyBaseBtn.anchor.y = 1;  // Anchor on bottom left corner
+        var armyBaseText = MainGame.game.make.text(0, -40, "Buy Army Base\n $30", style);
+        armyBaseText.anchor.y = 1;
+        buyArmyBaseBtn.addChild(armyBaseText);
         militaryGroup.addChild(buyArmyBaseBtn);
 
         // Toggles visibility of buildMenu
