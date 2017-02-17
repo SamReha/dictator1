3c7a334074a9a328ecd08b80ca7aa0fe27c83a00
= <=>
2017-02-17 01:45:59 +0000

    updatePopulation tracks basic stats turn to turn

--- updatePopulation.js
+++ updatePopulation.js
@@ -2,100 +2,65 @@
 var updatePopulation = function() {
 	/* global MainGame */
 	var pop = MainGame.population;
-	var workMap = pop.getWorkMap;
-	var houseMap = pop.getHouseMap;
+	//var workMap = pop.getWorkMap();
+	//var houseMap = pop.getHouseMap();
 
-	for (var person in pop.lowList) {
-		console.log(person);
-		console.log(MainGame.board.at(homeIndex));
+	for (var personIndex in pop.lowList()) {
+		var person = pop.at(personIndex);
 		var homeIndex = person.home;
-		var home = MainGame.board.at(homeIndex).building;
-
-		// Get new health
-		var totalBuildingHealth = 0;
-		var allHealthBuildingIndexes = MainGame.board.findBuilding(null, "health");
 		
-		for (var i in allHealthBuildingIndexes) {
-			var buildingData = MainGame.board.at(i).building.buildingData;
-			
-			// If the distance between the two buildings is <= the range of the healthBuilding, accumulate the health
-			for (var effect in buildingData.effects) {
-				if (effect.type != "health") continue;
-				if (MainGame.board.distanceOf(homeIndex, i) <= effect.range) {
-					var outPut = effect.outputTable[buildingData.people];
-					totalBuildingHealth += outPut;
-				}
-			}
-			
-			// If we've breached 100 health, just stop counting
-			if (totalBuildingHealth >= 100) {
-				totalBuildingHealth = 100;
-				break;
-			}
-		}
-		person.health = totalBuildingHealth;
-
-		// Get new shelter
-		console.log(home);
-		person.shelter = home.shelter;
-
-		// Get new education
-		var totalBuildingEdu = 0;
-		var allEduBuildingIndexes = MainGame.board.findBuilding(null, "education");
+		//console.log("updatePopulation health: " + person.health + " education: " + person.education + " shelter: " + person.shelter);
 		
-		for (var i in allEduBuildingIndexes) {
-			var buildingData = MainGame.board.at(i).building.buildingData;
-			
-			// If the distance between the two buildings is <= the range of the eduBuilding, accumulate education
-			for (var effect in buildingData.effects) {
-				if (effect.type != "education") continue;
-				if (MainGame.board.distanceOf(homeIndex, i) <= effect.range) {
-					var outPut = effect.outputTable[buildingData.people];
-					totalBuildingEdu += outPut;
-				}
-			}
-			
-			// If we've breached 100 education, just stop counting
-			if (totalBuildingEdu >= 100) {
-				totalBuildingEdu = 100;
-				break;
+		// This check can be safely ignored once shanty towns are spawning correctly
+		if (MainGame.board.at(homeIndex) != undefined) {
+			var home = MainGame.board.at(homeIndex).building;
+	
+			// Get new health
+			var totalBuildingHealth = getEffectOutputInRangeByType(homeIndex, "health");
+			person.health = totalBuildingHealth;
+	
+			// Get new shelter
+			person.shelter = home.maxShelter;
+	
+			// Get new education
+			var totalBuildingEdu = getEffectOutputInRangeByType(homeIndex, "education");
+			if (person.education < totalBuildingEdu) {
+				person.education = clampedSum(person.education, person.learningSpeed, totalBuildingEdu);
 			}
 		}
-		if (person.education < totalBuildingEdu) {
-			person.education = clampedSum(person.education, person.learningSpeed, totalBuildingEdu);
-		}
-	}
-
-	for (person in pop.midList) {
-		var homeIndex = person.home;
-		var home = MainGame.board.at(homeIndex).building;
-
-		// Get new health
-		person.health = home.health;
-
-		// Get new shelter
-		person.shelter = home.shelter;
-
-		// Get new education
-		person.education = home.education;
-	}
-
-	for (person in pop.highList) {
-		var homeIndex = person.home;
-		var home = MainGame.board.at(homeIndex).building;
-
-		// Get new health
-		person.health = home.health;
-
-		// Get new shelter
-		person.shelter = home.shelter;
-
-		// Get new education
-		person.education = home.education;
+		
+		// Make sure the global pop array gets updated
+		pop.people[personIndex] = person;
 	}
 }
 
 var clampedSum = function(a, b, max) {
 	var sum = a + b;
 	return sum > max ? max : sum;
+}
+
+var getEffectOutputInRangeByType = function(homeIndex, type) {
+	var totalOutput = 0;
+	var allBuildingIndexes = MainGame.board.findBuilding(null, type);
+	
+	for (var i in allBuildingIndexes) {
+		var buildingData = MainGame.board.at(i).building.buildingData;
+		
+		// If the distance between the two buildings is <= the range of the eduBuilding, accumulate education
+		for (var effect in buildingData.effects) {
+			if (effect.type != type) continue;
+			if (MainGame.board.distanceOf(homeIndex, i) <= effect.range) {
+				var outPut = effect.outputTable[buildingData.people];
+				totalOutput += outPut;
+			}
+		}
+		
+		// If we've breached 100%, just stop counting
+		if (totalOutput >= 100) {
+			totalOutput = 100;
+			break;
+		}
+	}
+	
+	return totalOutput;
 }
