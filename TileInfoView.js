6bfbc50fa30144deddc50da2389b2b2206d2d2a4
YinYi <kthtes@gmail.com>
2017-02-27 16:13:42 -0800

    Simpified the creation of the mouse-click menu

--- TileInfoView.js
+++ TileInfoView.js
@@ -1,25 +1,31 @@
+// fonts list
 var fontBrief=[
 	{font:"30px myKaiti", fill:"white", boundsAlignH:"top", boundsAlignV:"middle"},
 	{font:"30px myKaiti", fill:"white", boundsAlignH:"top", boundsAlignV:"middle"},
 	{font:"30px myKaiti", fill:"lightgreen", boundsAlignH:"top", boundsAlignV:"middle"}
 ];
+var fontDetail=[
+	{font:"35px myKaiti", fill:"white", boundsAlignH:"top", boundsAlignV:"middle"},
+	{font:"30px myKaiti", fill:"white", boundsAlignH:"top", boundsAlignV:"middle"}
+];
 
 var TileBriefInfoView={
     style:{font: "30px myKaiti", fill: "#ffffff", wordWrap: true, wordWrapWidth: 500, boundsAlignH: "center", boundsAlignV: "middle" , backgroundColor: "#ffff00" },
 	createNew: function(index){
+		console.assert(index || index===0);
 		/* global MainGame */
 		var game=MainGame.game;
 		// bg
 		var view=game.add.sprite(0,0,"tile_hover_backpanel");
 		// label position
-		var centerPos={x:view.texture.width*0.5, y:view.texture.height*0.5-30};
+		var startPos={x:view.texture.width*0.5, y:view.texture.height*0.5-30};
 		// tile index
 		view.index=index;
 
 		// creates 3 lines
 		var lines=["terrain", "building", "people"];
 		for(var i=0;i<lines.length;i++){
-			var oneLine=game.make.text(centerPos.x, centerPos.y+35*i,"",fontBrief[i]);
+			var oneLine=game.make.text(startPos.x, startPos.y+35*i,"",fontBrief[i]);
 			oneLine.anchor.set(0.5);
 			view.addChild(oneLine);
 			view[lines[i]]=oneLine;
@@ -73,160 +79,141 @@ var TileBriefInfoView={
 
 var TileDetailInfoView={
     createNew: function(index) {
-    	console.assert(index);
-		var style = { font: "20px STKaiti", fill: "#ffffff", wordWrap: true, wordWrapWidth: 500, boundsAlignH: "center", boundsAlignV: "middle" , backgroundColor: "#ffff00" };
+    	console.assert(index || index===0);
 
         /*global MainGame*/
+        var game=MainGame.game;
         var board = MainGame.board;
 
-        var bdiView = MainGame.game.add.sprite(0, 0, 'building_detail_backpanel');
+        var view = game.add.sprite(0, 0, 'building_detail_backpanel');
         var bdInputPriority = 2;
 
         // Class vars
-        bdiView.name = "BuildingDetail";
-        bdiView.index = index;		// Need explanation for what makes these two different
-        //bdiView.activeIndex = null;
-        // bdiView.loopingTimer = MainGame.game.time.events.loop(50, MapSelector.updateAll, buildingDetail, buildingDetail);
+        view.name = "BuildingDetail";
+        view.index = index;		// Need explanation for what makes these two different
 
         // bg (the grad)
-        var labelX = Math.floor(bdiView.texture.width / 2);
-        var labelY = Math.floor(bdiView.texture.height);
-        bdiView.y -= labelY;
+        var startPos={x:10, y:0};
 
         // label (bld name/lv)
-        bdiView.label = MainGame.game.make.text(labelX, 20, "", style, bdiView);
-        bdiView.label.anchor.x = 0.5;
-        bdiView.label.anchor.y = 0.5;
-        bdiView.addChild(bdiView.label);
-
-        // label2 (people)
-        bdiView.label2 = MainGame.game.make.text(10, 60, "", style, bdiView);
-        bdiView.label2.anchor.y = 0.5;
-        bdiView.addChild(bdiView.label2);
-
-        // label3 (health)
-        bdiView.label3=MainGame.game.make.text(10, 90,"",style,bdiView);
-        bdiView.label3.anchor.y = 0.5;
-        bdiView.addChild(bdiView.label3);
-
-        // label3 (health)
-        bdiView.label4=MainGame.game.make.text(10, 120,"",style,bdiView);
-        bdiView.label4.anchor.y = 0.5;
-        bdiView.addChild(bdiView.label4);
-
-        // label3 (health)
-        bdiView.label5=MainGame.game.make.text(10, 150,"",style,bdiView);
-        bdiView.label5.anchor.y = 0.5;
-        bdiView.addChild(bdiView.label5);
+        var lines=["building","people","health","education","shelter"];
+        for(var i=0;i<lines.length;i++){
+        	var fontIndex=(i===0?0:1);
+        	var oneLine=game.make.text(startPos.x, startPos.y+30*i, fontDetail[fontIndex]);
+        	if(i===0)
+        		oneLine.anchor.set(0.5);
+        	view.addChild(oneLine);
+        	view[lines[i]]=oneLine;
+        }
 
         // Hire button
         if(board.at(index).hasBuilding()){
-	        bdiView.addPersonButton = MainGame.game.make.button(30, labelY-50, "btnHire", 
+	        view.addPersonButton = game.make.button(30, startPos.y-50, "btnHire", 
 	            function() {
-	                //console.log("[MapSelector] Hire people for index: ",bdiView.curIndex);
+	                //console.log("[MapSelector] Hire people for index: ",view.curIndex);
 	                // TODO
 
 	                /*global MainGame*/
-	                var bld = MainGame.board.at(bdiView.index).building;
+	                var bld = MainGame.board.at(view.index).building;
 	                if (bld.people >= bld.maxPeople)
 	                    return;
 	                
 	                //console.log("[MapSelector] and the building's type/name is:["+bld.type+","+bld.name+"]");
-	                MainGame.population.hire(bdiView.index);
+	                MainGame.population.hire(view.index);
 	                //bld.people=bld.people+actual; [this is now done in building.addPerson()]
 	                // update display
-	                bdiView.label2.text="People: "+bld.people+"/"+bld.maxPeople;
+	                view.people.text="People: "+bld.people+"/"+bld.maxPeople;
 
 	                for(var outIndex=0;outIndex<bld.effects.length;++outIndex){
 	                    var outType = bld.effects[outIndex].type;
 	                    if(outType==="health"){ 
 	                        outType="Health";
 	                        /*global updateHomesNearOutput*/
-	                        updateHomesNearOutput(bdiView.index);
+	                        updateHomesNearOutput(view.index);
 	                    }else if(outType==="education"){
 	                        outType="Edu";
 	                        /*global updateHomesNearOutput*/
-	                        updateHomesNearOutput(bdiView.index);
+	                        updateHomesNearOutput(view.index);
 	                    }else if(outType==="freedom"){
 	                        outType="Extra Freedom";
 	                        /*global updateHomesNearOutput*/
-	                        updateHomesNearOutput(bdiView.index);
+	                        updateHomesNearOutput(view.index);
 	                    }else if(outType==="unrest"){
 	                        outType="Extra Unrest";
 	                        /*global updateHomesNearOutput*/
-	                        updateHomesNearOutput(bdiView.index);
+	                        updateHomesNearOutput(view.index);
 	                    }else if(outType==="money"){    outType="Money";    }
 
 	                    if(outIndex===0){
-	                        bdiView.label3.text=outType+" Output: "+bld.effects[outIndex].outputTable[bld.people];
+	                        view.health.text=outType+" Output: "+bld.effects[outIndex].outputTable[bld.people];
 	                    }else if(outIndex===1){
-	                        bdiView.label4.text=outType+" Output: "+bld.effects[outIndex].outputTable[bld.people];
+	                        view.education.text=outType+" Output: "+bld.effects[outIndex].outputTable[bld.people];
 	                    }else if(outIndex===2){
-	                        bdiView.label5.text=outType+" Output: "+bld.effects[outIndex].outputTable[bld.people];
+	                        view.shelter.text=outType+" Output: "+bld.effects[outIndex].outputTable[bld.people];
 	                    }
 	                }
 	                /*global updatePopulation*/
 	                updatePopulation(false,false);
-	            }, bdiView, 0, 1, 2, 3);
-	        bdiView.addPersonButton.input.priorityID = bdInputPriority;
-	        bdiView.addChild(bdiView.addPersonButton);
+	            }, view, 0, 1, 2, 3);
+	        view.addPersonButton.input.priorityID = bdInputPriority;
+	        view.addChild(view.addPersonButton);
 
 	        // Fire button
-	        bdiView.removePersonButton = MainGame.game.make.button(100, labelY-50, "btnFire",
+	        view.removePersonButton = game.make.button(100, startPos.y-50, "btnFire",
 	            function() {
 	                /*global MainGame*/
-	                var bld=MainGame.board.at(bdiView.index).building;
+	                var bld=MainGame.board.at(view.index).building;
 	                if(bld.people<=0)
 	                    return;
 	                
-	                MainGame.population.fire(bdiView.index);
+	                MainGame.population.fire(view.index);
 
 	                // update display
-	                bdiView.label2.text="People: "+bld.people+"/"+bld.maxPeople;
+	                view.people.text="People: "+bld.people+"/"+bld.maxPeople;
 
 	                for(var outIndex = 0; outIndex < bld.effects.length; outIndex++) {
 	                    var outType = bld.effects[outIndex].type;
 	                    if(outType==="health"){ 
 	                        outType="Health";
 	                        /*global updateHomesNearOutput*/
-	                        updateHomesNearOutput(bdiView.index);
+	                        updateHomesNearOutput(view.index);
 	                    }else if(outType==="education"){
 	                        outType="Edu";
 	                        /*global updateHomesNearOutput*/
-	                        updateHomesNearOutput(bdiView.index);
+	                        updateHomesNearOutput(view.index);
 	                    }else if(outType==="freedom"){
 	                        outType="Extra Freedom";
 	                        /*global updateHomesNearOutput*/
-	                        updateHomesNearOutput(bdiView.index);
+	                        updateHomesNearOutput(view.index);
 	                    }else if(outType==="unrest"){
 	                        outType="Extra Unrest";
 	                        /*global updateHomesNearOutput*/
-	                        updateHomesNearOutput(bdiView.index);
+	                        updateHomesNearOutput(view.index);
 	                    }else if(outType==="money"){
 	                        outType="Money";
 	                        MainGame.global.updateMoneyPerTurn();                    }
 
 	                    if(outIndex===0){
-	                        bdiView.label3.text=outType+" Output: "+bld.effects[outIndex].outputTable[bld.people];
+	                        view.health.text=outType+" Output: "+bld.effects[outIndex].outputTable[bld.people];
 	                    }else if(outIndex===1){
-	                        bdiView.label4.text=outType+" Output: "+bld.effects[outIndex].outputTable[bld.people];
+	                        view.education.text=outType+" Output: "+bld.effects[outIndex].outputTable[bld.people];
 	                    }else if(outIndex===2){
-	                        bdiView.label5.text=outType+" Output: "+bld.effects[outIndex].outputTable[bld.people];
+	                        view.shelter.text=outType+" Output: "+bld.effects[outIndex].outputTable[bld.people];
 	                    }
 	                }
 	                /*global updatePopulation*/
 	                updatePopulation(false,false);
-	            }, bdiView, 0,1,2,3);
-	        bdiView.removePersonButton.input.priorityID = bdInputPriority;
-	        bdiView.addChild(bdiView.removePersonButton);
+	            }, view, 0,1,2,3);
+	        view.removePersonButton.input.priorityID = bdInputPriority;
+	        view.addChild(view.removePersonButton);
     	}
 		// Class func
-		bdiView.updateInfo=function(tile){return TileDetailInfoView.updateInfo(bdiView,tile)};
-		bdiView.updatePos=function(){return TileDetailInfoView.updatePos(bdiView)};
+		view.updateInfo=function(tile){return TileDetailInfoView.updateInfo(view,tile)};
+		view.updatePos=function(){return TileDetailInfoView.updatePos(view)};
 
-        return bdiView;
+        return view;
 	},
-	updateInfo: function(bdiView, tile){
+	updateInfo: function(view, tile){
         if(!tile.hasBuilding())
             return;
 
@@ -241,15 +228,15 @@ var TileDetailInfoView={
             // If this terrain has a natural resource, display that, otherwise display the terrain name
             displayName = tile.getRes().key === '__default' ? tile.terrain.key : tile.getRes().key;
 
-            bdiView.label2.text = ''; // Make sure this text gets cleared if it's not going to be used
+            view.people.text = ''; // Make sure this text gets cleared if it's not going to be used
         } else {
             displayName = bld.name;// + " Lv"+bld.level;
 
             // Most buildings can contain people, but some (like roads) cannot. Be sure to correct for that.
             if (bld.subtype === 'road') {
-                bdiView.label2.text = '';
+                view.people.text = '';
             } else {
-                bdiView.label2.text = "People: " + bld.people + "/" + bld.maxPeople;
+                view.people.text = "People: " + bld.people + "/" + bld.maxPeople;
             }
 
             var str3="";
@@ -283,20 +270,20 @@ var TileDetailInfoView={
                     }
                 }
             }
-            bdiView.label3.text=str3;
-            bdiView.label4.text=str4;
-            bdiView.label5.text=str5;
+            view.health.text=str3;
+            view.education.text=str4;
+            view.shelter.text=str5;
         }
 
-        bdiView.label.text = displayName;
+        view.building.text = displayName;
 	},
-	updatePos: function(bdiView){
+	updatePos: function(view){
 		/*global MainGame*/
 		var board=MainGame.board;
-		var rect=board.rectOf(bdiView.index,board.currentScale);
+		var rect=board.rectOf(view.index,board.currentScale);
 
-        bdiView.x=rect.x-board._offset.x+rect.w*.85;
-        bdiView.y=rect.y-board._offset.y;
-        bdiView.scale.set(board.currentScale);
+        view.x=rect.x-board._offset.x+rect.w*.85;
+        view.y=rect.y-board._offset.y;
+        view.scale.set(board.currentScale);
 	}
 };
