a1378eacfd06f33e18e96eb91597e681edf154e8
Yi YIN <kthtes@gmail.com>
2017-03-09 14:54:58 -0800

    Minor changes

--- PeopleView.js
+++ PeopleView.js
@@ -1,44 +1,54 @@
 /* global MainGame */
+var DPageIndicator={
+	createNew: function(textures, maxPages){		
+		var v=MainGame.game.add.group();
+		// 
+		return v;
+	},
+
+};
 
 var DListView={
 	// ****************** Attention ****************** //	
-	// textureInfo: {normal, *high, *disabled};
-	// itemInfo: {size:{w,h}, *clickedCallback, *change};
-	// *marginInfo: {*l, *r, *t, *b};
-	// *isVertical
-	// * === optional
-	createNew: function(textureInfo, marginInfo, itemInfo, isVertical){
+	// textures: {normal, *high, *disabled}
+	// margin: {*l, *r, *t, *b}
+	// itemSize: {w, h}
+	// *itemCallback: function(index, *prevIndex)
+	// *isHorizontal: true or false
+	// (* === optional)
+	createNew: function(textures, margin, itemSize, itemCallback, isHorizontal){
 		console.log("DListView: creating a list view requires:");
-		console.log("textureInfo:{normal:,*high:,*disabled}, itemInfo:{size,count}, *marginInfo:{*l,*r,*t,*b} and *isVertical");
+		console.log("textures:{normal:,*high:,*disabled}, itemSize:{size,count}, *margin:{*l,*r,*t,*b} and *isHorizontal");
 		// set the normal texture
-		var v=MainGame.game.add.sprite(0,0,textureInfo.normal);
-		// store the vars
-		v.textureInfo=JSON.parse(JSON.stringify(textureInfo));
-		v.itemInfo=JSON.parse(JSON.stringify(itemInfo));
-		v.marginInfo=JSON.parse(JSON.stringify(itemInfo));
-		v.isVertical=isVertical;
-		v.itemSelectedCallback=itemSelectedCallback;
-		// set the state to "normal"
+		var v=MainGame.game.add.sprite(0,0,textures.normal);
 		v.state="normal";
+		// store the vars
+		v.textures=JSON.parse(JSON.stringify(textures));
+		v.margin=JSON.parse(JSON.stringify(margin));
+		v.itemSize=JSON.parse(JSON.stringify(itemSize));
+		v.itemCallback=itemCallback;
+		v.isHorizontal=isHorizontal;
 		// create the children group that contains all children
 		v.childrenGroup=MainGame.game.make.group();
-		v.childrenGroup.x=marginInfo.l?marginInfo.l:0;
-		v.childrenGroup.y=marginInfo.t?marginInfo.t:0;
+		v.childrenGroup.x=margin.l?margin.l:0;
+		v.childrenGroup.y=margin.t?margin.t:0;
 		v.addChild(v.childrenGroup);
 
 		// enable the input for v
 		v.prevSelected=null;
 		v.selected=null;
 		v.inputEnabled=true;
-        v.events.onInputUp.add(DListView.onItemSelected,v);
+        v.events.onInputUp.add(DListView.onClicked,v);
 
 		// Class func
 		// returns the [i]th item
 		v.at=function(i){return v.childrenGroup[i]};
 		// adds an item [itemSprite] at [i]
-		v.addItem=function(itemSprite){return DListView.addItem(v,itemSprite)};
+		v.add=function(itemSprite){return DListView.add(v,itemSprite)};
 		// removes an item at [i]
-		v.removeItemAt=function(index){return v.childrenGroup.removeChildAt(index)};
+		v.removeAt=function(index){return v.childrenGroup.removeChildAt(index).destroy()};
+		// remvoes all items
+		v.removeAll=function(){return v.childrenGroup.removeAll(true)};
 		// returns the item count
 		v.itemCount=function(){return v.childrenGroup.length};
 		// returns the *index* of the selected item
@@ -52,13 +62,13 @@ var DListView={
 
 		return v;
 	},
-	addItem: function(view, itemSprite){
+	add: function(view, itemSprite){
 		var index=view.childrenGroup.length;
 		view.childrenGroup.addChild(itemSprite);
-		if(view.isVertical){
-			itemSprite.y=index*view.itemInfo.size.h;
+		if(view.isHorizontal){
+			itemSprite.y=index*view.itemSize.h;
 		}else{
-			itemSprite.x=index*view.itemInfo.size.w;
+			itemSprite.x=index*view.itemSize.w;
 		}
 	},
 	setState: function(view, state){
@@ -66,18 +76,19 @@ var DListView={
 		if(view.state===state)
 			return;
 		view.state=state;
-		if(view.textureInfo[state])
-			view.loadTexture(view.textureInfo[state]);
+		if(view.textures[state])
+			view.loadTexture(view.textures[state]);
+		else
+			view.loadTexture(view.textures.normal);
 	},
 	hitTest: function(view, px, py, isLocal){
 		var pos={x:px, y:py};
 		if(!isLocal)
 			pos={x:pos.x-view.x-view.childrenGroup.x, y:pos.y-view.y-view.childrenGroup.y};
 		// now pos is the local position relative to view.childrenGroup
-		var itemSize=view.itemInfo.size;
-		var itemCount=view.itemInfo.count;
+		var itemSize=view.itemSize;
 		var index=0;
-		if(view.isVertical)
+		if(view.isHorizontal)
 			index=Math.floor(pos.y/itemSize.h);			
 		else
 			index=Math.floor(pos.x/itemSize.w);
@@ -86,13 +97,15 @@ var DListView={
 		else
 			return index;
 	},
-	onItemSelected: function(){
+	onClicked: function(){
 		// get index
-		this.prevSelected=this.selected;
-		this.selected=DListView.hitTest(this, this.game.input.x, this.game.input.y, false);
+		var sel=DListView.hitTest(this, this.game.input.x, this.game.input.y, false);
 		// call the callback func with index
-		if(this.selected)
-			this.itemSelectedCallback(this.prevSelected,this.selected);
+		if(sel===0 || sel){
+			this.prevSelected=this.selected;
+			this.selected=sel;
+			this.itemCallback(this.selected, this.prevSelected);
+		}
 	},
 };
 
@@ -101,23 +114,23 @@ var PeopleView={
 		var pv=MainGame.game.add.sprite(0,0,'peopleViewBg');
 
 		pv.left=DListView.createNew(
-			{normal:"peopleViewLeftBg"}, 
+			{normal:"peopleViewLeftBg",high:"peopleViewRightBg"}, 
 			{l:56,t:50}, 
-			{size:{w:200,h:100}}, 
-			true, 
+			{w:200,h:50}, 
 			function(prev,cur){
 				console.log("Item selected: prev, cur="+prev+","+cur);
-			}
+			},
+			true
 		);
 		pv.addChild(pv.left);
 
 		// test: create 3 texts
 		var text1=MainGame.game.make.text(0,0,"Text1");
-		pv.left.addItem(text1,0);
+		pv.left.add(text1,0);
 		var text2=MainGame.game.make.text(0,0,"Text2");
-		pv.left.addItem(text2,1);
+		pv.left.add(text2,1);
 		var text3=MainGame.game.make.text(0,0,"Text3");
-		pv.left.addItem(text3,2);
+		pv.left.add(text3,2);
 
 		// Class funcs
 		pv.setVisible=function(value){pv.visible=value};
