f85e39ab2c0465e53eb87894c44346f1419bd243
Yi YIN <kthtes@gmail.com>
2017-03-10 11:06:22 -0800

    minor bug fixed

--- PeopleView.js
+++ PeopleView.js
@@ -4,7 +4,7 @@
 var PeopleRightView={
 	style: {font:"30px myKaiti", fill:"black"},
 	createNew: function(data){
-		var v=MainGame.game.add.sprite(0,0,"peopleViewRightBg");
+		var v=MainGame.game.make.sprite(0,0,"peopleViewRightBg");
 		// name, health, edu, shelter
 		v.data=JSON.parse(JSON.stringify(data));
 		// ListView: 10 items (slots)
@@ -15,13 +15,15 @@ var PeopleRightView={
 			{w:400, h:50},
 			function(index){PeopleRightView.onPersonSelected(v,index)}
 		);
+		v.addChild(v.listView);
 		// PageIndicator: N pages
 		var pageCount=Math.ceil(data.length/10);
 		v.pageIndicator=DPageIndicator.createNew(
 			pageCount,
 			{width:400,textPos:{x:200,y:500}},
 			function(index){PeopleRightView.onPageChanged(v,index)}
-		);		
+		);
+		v.addChild(v.pageIndicator);
 		// setup the init page
 		PeopleRightView._setupPage_(v,0);
 		return v;
@@ -45,23 +47,23 @@ var PeopleRightView={
 	_setupPage_: function(view,pageIndex){
 		view.listView.removeAll();
 		var startIndex=pageIndex*10;
-		var endIndex=Math.min(startIndex+9,view.data.length);
-		for(var i=startIndex;i<=endIndex;i++){
+		var endIndex=Math.min(startIndex+10,view.data.length);
+		for(var i=startIndex;i<endIndex;i++)
 			view.listView.add(PeopleRightView._makeEntry_(view.data[i]));
-		}
 	},
 };
 
 var PeopleView={
 	createNew: function(){
 		var pv=MainGame.game.add.sprite(0,0,'peopleViewBg');
+		pv.x=100, pv.y=100;
 
 		// create low people view (right)
 			// create low people data
 		var lowData=[{name:"Sam",health:50,edu:50,shelter:50}];
 		pv.right=PeopleRightView.createNew(lowData);
+		pv.right.x=100, pv.right.y=150;
 		// TBD: set a precise x
-		pv.right.x=400;
 		pv.addChild(pv.right);
 
 		// Class funcs
