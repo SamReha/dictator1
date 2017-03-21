10304fe29d6e42a2db1f2d7476b0462757b15c99
JSBaker <jsbaker73@gmail.com>
2017-03-20 19:33:07 -0700

    Finished contract view

--- PeopleContractView.js
+++ PeopleContractView.js
@@ -11,34 +11,25 @@ var PeopleContractView={
 		v.inputEnabled=true;
 		v.input.priorityID=120;
 		v.newPayLevel=(personDataRef.payLevel?personDataRef.payLevel:0);
-		var title = "";
-		var job = "";
 		// TODO: adjust layout!!
 		// setup its elements
 			// port & name
 		var textureString;
-
 		switch (personDataRef.role) {
 			case Person.Bureaucrat:
-				title = personDataRef.type===1?"Bureaucrat":"Minister of Bureaucracy";
-				job = "Minister of Bureaucracy";
 				textureString = 'bureaucrat_port_' + personDataRef.portIndex;
 				break;
 			case Person.Merchant:
-				title = personDataRef.type===1?"Financier":"Minister of Finance";
-				job = "Minister of Finance";
 				textureString = 'merchant_port_' + personDataRef.portIndex;
 				break;
 			case Person.Military:
-				title = personDataRef.type===1?"Military Officer":"Minister of the Military";
-				job = "Minister of the Military";
 				textureString = 'military_port_' + personDataRef.portIndex;
 				break;
 			default:
 				break;
 		}
 
-		// // ----Portrait, Name, and Title----
+		// ----Portrait, Name, and Title----
 		v.port = MainGame.game.make.sprite((v.width/8), (v.height/9), textureString);
 		v.port.anchor.setTo(0.5,0.5);
 		v.port.scale.setTo(1.5,1.5);
@@ -48,38 +39,34 @@ var PeopleContractView={
 		v.nameLabel.anchor.setTo(0,0.5);
 		v.addChild(v.nameLabel);
 
-		v.roleLabel=MainGame.game.make.text((v.width/4),(v.height/7),title,PeopleContractView.contractStyle);
+		v.roleLabel=MainGame.game.make.text((v.width/4),(v.height/7),"",PeopleContractView.contractStyle);
+		// set in updateSelf
 		v.roleLabel.anchor.setTo(0,0.5);
 		v.addChild(v.roleLabel);
 
 		// ----Current Payment----
-		v.currentPayLabel=MainGame.game.make.text((v.width*1/16), (v.height*13/48), "",PeopleContractView.contractStyle);
+		v.currentPayLabel=MainGame.game.make.text((v.width*3/64), (v.height*13/48), "",PeopleContractView.contractStyle);
 		v.currentPayLabel.anchor.setTo(0,0.5);
 		v.currentPay=MainGame.game.make.text(0, 0, "",PeopleContractView.contractStyle);
 		v.currentPay.anchor.setTo(0.5,0.5);
-		if(v.dataRef.type===Person.Hi){
-			v.currentPayLabel.text=personDataRef.name+"'s current annual salary is ________.";
-			v.currentPay=PeopleContractView.pays[personDataRef.payLevel];
-			v.currentPay.x=(v.currentPayLabel.width*7/8);
-		}else{
-			console.log("Infulential");
-			v.currentPayLabel.text="___________________ is available to be hired\n  as a new "+job+"."
-			v.currentPay.text=personDataRef.name;
-			v.currentPay.x=(v.currentPayLabel.width*3/12); v.currentPay.y=(v.currentPayLabel.height*-7/24);
-		}
+		v.currentPayName=MainGame.game.make.text(0, 0, "",PeopleContractView.contractStyle);
+		v.currentPayName.anchor.setTo(0.5,0.5);
+		// set in updateSelf
 		v.addChild(v.currentPayLabel);
 		v.currentPayLabel.addChild(v.currentPay);
+		v.currentPayLabel.addChild(v.currentPayName);
 
-		// // ----Expected Payment----
-		v.expectedPayLabel=MainGame.game.make.text((v.width*1/16), (v.height*13/30), "Ministers with a similar amount of Influence\n  are expected to be paid ________ annually.",PeopleContractView.contractStyle);
+		// ----Expected Payment----
+		v.expectedPayLabel=MainGame.game.make.text((v.width*3/64), (v.height*13/30), "Ministers with a similar amount of Influence\n  are expected to be paid ________ annually.",PeopleContractView.contractStyle);
 		v.expectedPayLabel.anchor.setTo(0,0.5);
 		v.addChild(v.expectedPayLabel);
-		v.expectedPay=MainGame.game.make.text((v.expectedPayLabel.width*2/3), (v.expectedPayLabel.height*5/24), "$"+2+"K",PeopleContractView.contractStyle);
+		v.expectedPay=MainGame.game.make.text((v.expectedPayLabel.width*2/3), (v.expectedPayLabel.height*5/24), "",PeopleContractView.contractStyle);
+		// set in updateSelf
 		v.expectedPay.anchor.setTo(0.5,0.5);
 		v.expectedPayLabel.addChild(v.expectedPay);
 
 		// ----Set New Payment----
-		v.newPayLabel=MainGame.game.make.text((v.width*1/16),(v.height*27/48),"Set their new salary:       ________",PeopleContractView.contractStyle);
+		v.newPayLabel=MainGame.game.make.text((v.width*3/64),(v.height*27/48),"Set their new salary:       ________",PeopleContractView.contractStyle);
 		v.addChild(v.newPayLabel);
 
 		v.decButton=MainGame.game.make.button((v.newPayLabel.width*13/20),(v.newPayLabel.height*3/8),'redMinusButton',function(){
@@ -96,7 +83,8 @@ var PeopleContractView={
 		v.incButton.input.priorityID=121;
 		v.newPayLabel.addChild(v.incButton);
 
-		v.newPay=MainGame.game.make.text((v.newPayLabel.width*17/20),(v.newPayLabel.height*3/8),"$"+PeopleContractView.pays[v.newPayLevel]+"K",PeopleContractView.contractStyle);
+		v.newPay=MainGame.game.make.text((v.newPayLabel.width*17/20),(v.newPayLabel.height*3/8),"",PeopleContractView.contractStyle);
+		// set in updateSelf
 		v.newPay.anchor.setTo(0.5,0.5);
 		v.newPayLabel.addChild(v.newPay);
 
@@ -106,54 +94,29 @@ var PeopleContractView={
 			v.incButton.visible=false;
 
 		// ----Text Alerting Player to Important Information----
-		/*global MainGame*/
-		v.playerAlertLabel=MainGame.game.make.text((v.width*1/16),(v.height*3/4),"",PeopleContractView.contractStyle);
+		v.playerAlertLabel=MainGame.game.make.text((v.width*3/64),(v.height*3/4),"",PeopleContractView.contractStyle);
 		v.playerAlertLabel.anchor.setTo(0,0.5);
 		v.playerAlert1=MainGame.game.make.text(0,0,"",PeopleContractView.contractStyle);
 		v.playerAlert1.anchor.setTo(0.5,0.5);
 		v.playerAlert2=MainGame.game.make.text(0,0,"",PeopleContractView.contractStyle);
 		v.playerAlert2.anchor.setTo(0.5,0.5);
-
-		if(personDataRef.type===2){
-			// ----Contract Renewal Deadline----
-			v.playerAlertLabel.text="Renewing this contract will extend\n  the renwal deadline from ________ to ________.";
-			v.playerAlert1.text=(1950+MainGame.global.turn-1)+personDataRef.renewCount;
-			v.playerAlert1.x=(v.playerAlertLabel.width*1/2); v.playerAlert1.y=(v.playerAlertLabel.height*1/2);
-			v.playerAlert2.text=(1950+MainGame.global.turn-1)+4;
-			v.playerAlert2.x=(v.playerAlertLabel.width*3/4); v.playerAlert2.y=(v.playerAlertLabel.height*1/2);
-		}else{
-			// ----Current Minister of This Role----
-			var minister = MainGame.population.typeRoleList(Person.Hi,personDataRef.role);
-			if(minister.length>0){
-				v.playerAlertLabel.text="Hiring ___________________ as the new minister\n  will replace ___________________.";
-				v.playerAlert1.text=personDataRef.name;
-				v.playerAlert1.x=(v.playerAlertLabel.width*1/8); v.playerAlert1.y=(v.playerAlertLabel.height*1/4);
-				v.playerAlert2.text=minister[0].name;
-				v.playerAlert2.x=(v.playerAlertLabel.width*2/5); v.playerAlert2.y=(v.playerAlertLabel.height*3/4);
-			}else{
-				v.playerAlertLabel.text="Hiring ___________________ will not replace\n  any of the current ministers.";
-				v.playerAlert1.text=personDataRef.name;
-				v.playerAlert1.x=(v.playerAlertLabel.width*2/5); v.playerAlert1.y=(v.playerAlertLabel.height*-7/24);
-			}
-		}
+		// set in updateSelf
 		v.addChild(v.playerAlertLabel);
 		v.playerAlertLabel.addChild(v.playerAlert1);
 		v.playerAlertLabel.addChild(v.playerAlert2);
 
 		// ----Hire/Renew, Fire, and Cancel Buttons
-		if(personDataRef.type===2){
-			v.fireButton=MainGame.game.make.button((v.width*1/5),(v.height*8/9),'small_generic_button',function(){
-				v.fireButton.freezeFrames=true;
-				PeopleContractView.onWorkChanged(v,true);
-			},v.fireButton,0,1,0,0);
-			v.fireButton.anchor.setTo(0.5,0.5);
-			v.fireButton.input.priorityID=121;
-			v.addChild(v.fireButton);
-			v.fireText=MainGame.game.make.text(0,0,"Fire",PeopleContractView.styleButton);
-			v.fireText.anchor.x=0.5;
-			v.fireText.anchor.y=0.5;
-			v.fireButton.addChild(v.fireText);
-		}
+		v.fireButton=MainGame.game.make.button((v.width*1/5),(v.height*8/9),'small_generic_button',function(){
+			PeopleContractView.onWorkChanged(v,true);
+		},v.fireButton,0,1,0,0);
+		v.fireButton.anchor.setTo(0.5,0.5);
+		v.fireButton.input.priorityID=121;
+		v.addChild(v.fireButton);
+		v.fireText=MainGame.game.make.text(0,0,"Fire",PeopleContractView.styleButton);
+		v.fireText.anchor.x=0.5;
+		v.fireText.anchor.y=0.5;
+		v.fireButton.addChild(v.fireText);
+		// set visible in updateSelf
 
 		v.cancelButton=MainGame.game.add.button((v.width*1/2),(v.height*8/9),'small_generic_button',function(){
 			v.cancelButton.freezeFrames=true;
@@ -164,19 +127,19 @@ var PeopleContractView={
 		v.cancelButton.name="This is a cancle butto";
 		v.addChild(v.cancelButton);
 
-		v.cancelText=MainGame.game.make.text(0,0,"Return",PeopleContractView.styleButton);
+		v.cancelText=MainGame.game.make.text(0,0,"Close",PeopleContractView.styleButton);
 		v.cancelText.anchor.x=0.5;
 		v.cancelText.anchor.y=0.5;
 		v.cancelButton.addChild(v.cancelText);
 
 		v.hireButton=MainGame.game.make.button((v.width*4/5),(v.height*8/9),'small_generic_button',function(){
-			v.hireButton.freezeFrames=true;
 			PeopleContractView.onWorkChanged(v,false);
 		},v.hireButton,0,1,0,0);
 		v.hireButton.anchor.setTo(0.5,0.5);
 		v.hireButton.input.priorityID=121;
 		v.addChild(v.hireButton);
-		v.hireText=MainGame.game.make.text(0,0,personDataRef.type===1?"Hire":"Renew",PeopleContractView.styleButton);
+		v.hireText=MainGame.game.make.text(0,0,"",PeopleContractView.styleButton);
+		// set in updateSelf
 		v.hireText.anchor.x=0.5;
 		v.hireText.anchor.y=0.5;
 		v.hireButton.addChild(v.hireText);
@@ -184,6 +147,9 @@ var PeopleContractView={
 		// Class func
 		v.suicide=function(){PeopleContractView.suicide(v)};
 		v.updateSelf=function(){PeopleContractView.updateSelf(v)};
+
+
+		v.updateSelf();
 
 		return v;
 	},
@@ -205,10 +171,12 @@ var PeopleContractView={
 		if(isFire){
 			view.dataRef.payLevel=0;
 			view.dataRef.unSetHighClass();
+			view.dataRef.renewCount=null;
 		}
 		else{
 			view.dataRef.payLevel=view.newPayLevel;
 			view.dataRef.setHighClass(PeopleContractView.pays[view.dataRef.payLevel]);
+			view.dataRef.renewCount=4;
 		}
 		// Update the UI
 		MainGame.hud.coalitionFlag.updateSelf();
@@ -222,6 +190,80 @@ var PeopleContractView={
 	updateSelf: function(v){
 		// TODO: hide Hire, update Bure -> Ministor of Bure, etc.
 		console.log("Update itself.");
-		v.hireButton.visible=false;
+
+		var title = "";
+		var job = "";
+		switch (v.dataRef.role) {
+			case Person.Bureaucrat:
+				title = v.dataRef.type===1?"Bureaucrat":"Minister of Bureaucracy";
+				job = "Minister of Bureaucracy";
+				break;
+			case Person.Merchant:
+				title = v.dataRef.type===1?"Financier":"Minister of Finance";
+				job = "Minister of Finance";
+				break;
+			case Person.Military:
+				title = v.dataRef.type===1?"Military Officer":"Minister of the Military";
+				job = "Minister of the Military";
+				break;
+			default:
+				break;
+			}
+
+		// ----Portrait, Name, and Title----
+		v.roleLabel.text=title;
+
+		// ----Current Payment----
+		if(v.dataRef.type===Person.Hi){
+			v.currentPayLabel.text="___________________ is recieving an annual salary\n  of ________ as the "+title+".";
+			v.currentPay.text="$"+PeopleContractView.pays[v.dataRef.payLevel]+"K";
+			v.currentPay.x=(v.currentPayLabel.width*9/48); v.currentPay.y=(v.currentPayLabel.height*5/24);
+			v.currentPayName.text=v.dataRef.name;
+			v.currentPayName.x=(v.currentPayLabel.width*2/9); v.currentPayName.y=(v.currentPayLabel.height*-7/24);
+		}else{
+			console.log("Infulential");
+			v.currentPayLabel.text="___________________ is available to be hired\n  as a new "+job+"."
+			v.currentPay.text="";
+			v.currentPayName.text=v.dataRef.name;
+			v.currentPayName.x=(v.currentPayLabel.width*1/4); v.currentPayName.y=(v.currentPayLabel.height*-7/24);
+		}
+
+		// ----Expected Payment----
+		v.expectedPay.text="$"+2+"K";
+
+		// ----Set New Payment----
+		v.newPay.text="$"+PeopleContractView.pays[v.newPayLevel]+"K";
+
+		v.decButton.visible=(v.newPayLevel!==0);
+		v.incButton.visible=(v.newPayLevel!==PeopleContractView.pays.length-1);
+
+		// ----Text Alerting Player to Important Information----
+		if(v.dataRef.type===2){
+			// ----Contract Renewal Deadline----
+			v.playerAlertLabel.text="Renewing this contract will extend the renewal\n  deadline from ________ to ________.";
+			v.playerAlert1.text=(1950+MainGame.global.turn-1)+v.dataRef.renewCount;
+			v.playerAlert1.x=(v.playerAlertLabel.width*11/24); v.playerAlert1.y=(v.playerAlertLabel.height*5/24);
+			v.playerAlert2.text=(1950+MainGame.global.turn-1)+4;
+			v.playerAlert2.x=(v.playerAlertLabel.width*3/4); v.playerAlert2.y=(v.playerAlertLabel.height*5/24);
+		}else{
+			// ----Current Minister of This Role----
+			var minister = MainGame.population.typeRoleList(Person.Hi,v.dataRef.role);
+			if(minister.length>0){
+				v.playerAlertLabel.text="Hiring ___________________ as the new minister\n  will replace ___________________.";
+				v.playerAlert1.text=v.dataRef.name;
+				v.playerAlert1.x=(v.playerAlertLabel.width*13/36); v.playerAlert1.y=(v.playerAlertLabel.height*-7/24);
+				v.playerAlert2.text=minister[0].name;
+				v.playerAlert2.x=(v.playerAlertLabel.width*1/2); v.playerAlert2.y=(v.playerAlertLabel.height*5/24);
+			}else{
+				v.playerAlertLabel.text="Hiring ___________________ will not replace\n  any of the current ministers.";
+				v.playerAlert1.text=v.dataRef.name;
+				v.playerAlert1.x=(v.playerAlertLabel.width*2/5); v.playerAlert1.y=(v.playerAlertLabel.height*-7/24);
+				v.playerAlert2.text="";
+			}
+		}
+
+		// ----Hire/Renew, Fire, and Cancel Buttons
+		v.fireButton.visible=(v.dataRef.type===2);
+		v.hireText.text=v.dataRef.type===1?"Hire":"Renew";
 	}
 };
