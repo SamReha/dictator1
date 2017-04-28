86de2d6f59c5a22b6fc5d511bea808d21fe493f9
JSBaker <jsbaker73@gmail.com>
2017-04-27 17:52:55 -0700

    removed console logs from UnitAI

--- UnitAI.js
+++ UnitAI.js
@@ -3,27 +3,21 @@ var UnitAI = {
 	takeTurn: function(unit) {
 		//console.log(MainGame.global.turn, unit.name, unit.currentIndex);
 		if (unit.target === null) {
-			console.log(unit.number+" @ "+unit.currentIndex+" - selecting target");
 			unit.target = UnitAI.findTarget(unit);
 		}
 
-		console.log(unit.number+" @ "+unit.currentIndex+" - choosing a move");
 		var newMoveIndex = UnitAI.chooseMove(unit);
-		console.log(unit.number+" @ "+unit.currentIndex+" - new tile: "+newMoveIndex);
 		unit.move(newMoveIndex);
 		if (unit.isAttacking)
 			UnitAI.attackTarget(unit);
-		console.log(unit.number+" @ "+unit.currentIndex+" - done\n");
 		console.log("");
 	},
 
 	findTarget: function(unit) {
 		if (unit.type === Unit.Riot) {
 			if(unit.currentIndex === unit.origin && MainGame.board.at(unit.currentIndex).getBuilding().name !== 'rubble'){
-				console.log(unit.number+" @ "+unit.currentIndex+" - targeting origin");
 				return unit.origin;
 			} else {
-				console.log(unit.number+" @ "+unit.currentIndex+" - targeting palace");
 				return MainGame.board.findBuilding('palace',null,null,null)[0];
 			}
 		} else if (unit.type === Unit.Army) { // Should never call findTarget with a homeless group, but just to be sure
@@ -44,10 +38,8 @@ var UnitAI = {
 					}
 				}
 
-				console.log(unit.number+" @ "+unit.currentIndex+" - selecting Riot @ "+minIndex);
 				return MainGame.board.at(minIndex).getUnit();
 			} else {
-				console.log(unit.number+" @ "+unit.currentIndex+" - no target found");
 				return null;
 			}
 		}
@@ -58,7 +50,6 @@ var UnitAI = {
 		var targetIndex = null;
 		if (unit.type === Unit.Army) {
 			if (unit.target !== null) {
-				console.log(unit.number+" @ "+unit.currentIndex+" - has a target");
 				targetIndex = unit.target;
 			} else {
 				// If an Army unit does not have a target, then it will patrol roads
@@ -71,26 +62,20 @@ var UnitAI = {
 							roads.push(adjacent[i]);
 					}
 					if(roads.length>0){
-						console.log(unit.number+" @ "+unit.currentIndex+" - patrolling the road");
 						return roads[Math.floor(Math.random()*roads.length)];
 					}
 					else{
-						console.log(unit.number+" @ "+unit.currentIndex+" - no connecting roads, so targeting origin");
 						targetIndex = unit.origin;
 					}
 				}else{
-					console.log(unit.number+" @ "+unit.currentIndex+" - no connecting roads, so targeting origin");
 					targetIndex = unit.origin;
 				}
 			}
 		} else if (unit.type === Unit.Riot) {
 			if (unit.currentIndex === unit.target) {
-				console.log(unit.number+" @ "+unit.currentIndex+" - standing on target, so not moving");
 				unit.isAttacking = true;
-				console.log(unit.number+" @ "+unit.currentIndex+" - attack!");
 				return null;
 			}
-			console.log(unit.number+" @ "+unit.currentIndex+" - has a target");
 			targetIndex = unit.target;
 		}
 
@@ -102,15 +87,11 @@ var UnitAI = {
 			// Checking to see if the adjacent hex is the unit's target
 			// if so, then the unit will immediately attack it
 			if (adjacentTiles[i] === targetIndex) {
-				console.log(unit.number+" @ "+unit.currentIndex+" - target is adjacent");
 				if (unit.type === Unit.Army && unit.target != null) {
 					unit.isAttacking = true;
-					console.log(unit.number+" @ "+unit.currentIndex+" - attack!");
 					return null;
 				} else {
-					console.log(unit.number+" @ "+unit.currentIndex+" - move to target");
 					if(unit.type === Unit.Riot){
-						console.log(unit.number+" @ "+unit.currentIndex+" - attack!");
 						unit.isAttacking = true;
 					}
 					return adjacentTiles[i];
@@ -124,9 +105,7 @@ var UnitAI = {
 				choices[i].weight = 999;
 				var adjacentUnit = tile.getUnit();
 				if(unit.type === Unit.Riot && adjacentUnit.type === Unit.Riot && unit.health <= adjacentUnit.health && choices[i].weight < currentWeight){
-					console.log(unit.number+" @ "+unit.currentIndex+" - merging with adjacent Riot");
 					UnitAI.mergeUnits(adjacentUnit,unit);
-					console.log(unit.number+" @ "+unit.currentIndex+" - remaining people stand still");
 					return null;
 				}
 			}else if(tile.getTerrainType() !== "water" && tile.getTerrainType() !== "mountain"){
@@ -138,7 +117,6 @@ var UnitAI = {
 				else
 					choices[i].weight += Math.floor(Math.random()*50);
 			}
-			console.log(choices[i].tileIndex+" "+choices[i].weight+" "+choices[i].building+" "+tile.hasUnit());
 		}
 
 		// sort choices by their weighted values
@@ -147,20 +125,17 @@ var UnitAI = {
 		// targeting nearby building
 		if (unit.type === Unit.Riot) {
 			if(choices[0].building !== "road" && choices[0].building !== "rubble" && choices[0].building !== ""){
-				console.log(unit.number+" @ "+unit.currentIndex+" - changed target and attacking "+choices[0].building);
 				unit.isAttacking = true;
 				unit.target = choices[0].tileIndex;
 			}
 		}
 
-		console.log(unit.number+" @ "+unit.currentIndex+" - moving to "+choices[0].tileIndex);
 		return choices[0].tileIndex;
 	},
 	
 	attackTarget: function(unit) {
 		if (unit.type === Unit.Riot) {
 			var targetTile = MainGame.board.at(unit.target);
-			console.log(unit.number+" @ "+unit.currentIndex+" - health: "+unit.health+" vs. targetIntegrity: "+targetTile.getBuilding().integrity);
 
 			// If the building is about to die...
 			if(unit.health >= targetTile.getBuilding().integrity){
@@ -168,10 +143,8 @@ var UnitAI = {
 				unit.isAttacking = false;
 			}
 			targetTile.damageBuilding(unit.health);
-			console.log(unit.number+" @ "+unit.currentIndex+" - targetNewIntegrity: "+targetTile.getBuilding().integrity);
 		} else if (unit.type === Unit.Army) {
 			var targetUnit = unit.target;
-			console.log(unit.number+" @ "+unit.currentIndex+" - health: "+unit.health+" vs. targetHealth: "+targetUnit.health);
 
 			// If the targeted unit is about to die...
 			if(unit.health >= targetUnit.health){
@@ -179,14 +152,11 @@ var UnitAI = {
 				unit.isAttacking = false;
 			}
 			targetUnit.takeDamage(unit.health);
-			console.log(unit.number+" @ "+unit.currentIndex+" - targetNewHealth: "+targetUnit.health);
 		}
 	},
 
 	mergeUnits: function(unit1, unit2) {
 		var transfer = Math.min(unit2.health, Unit.maxSize - unit1.health);
-		console.log(unit2.number+" @ "+unit2.currentIndex+" - newhealth: "+(unit2.health-transfer));
-		console.log(unit1.number+" @ "+unit1.currentIndex+" - newhealth: "+(unit1.health-transfer));
 		unit1.addPeople(transfer);
 		unit2.subtractPeople(transfer);
 	}
