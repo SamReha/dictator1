6c24dfadf1354e688fce38475e1829cbcd7e9a3e
Sam Reha <sreha@ucsc.edu>
2017-05-05 20:10:43 -0700

    Enhanced mouse over stuff

--- TileInfoView.js
+++ TileInfoView.js
@@ -1,13 +1,8 @@
-var fontDetail=[
-	{font:"28px myKaiti", fill:"black", boundsAlignH:"top", boundsAlignV:"middle"},
-	{font:"24px myKaiti", fill:"black", boundsAlignH:"top", boundsAlignV:"middle"}
-];
-
 var TileBriefView = {
     // fonts list
     fontBrief: [
         {font: "40px myKaiti", fill: "white", boundsAlignH: "top", boundsAlignV: "middle"},
-        {font: "40px myKaiti", fill: "lightgreen", boundsAlignH: "top", boundsAlignV: "middle"}
+        {font: "30px myKaiti", fill: "orange", boundsAlignH: "top", boundsAlignV: "middle"}
     ],
 
 	createNew: function(index) {
@@ -17,14 +12,28 @@ var TileBriefView = {
 
         var tile = MainGame.board.at(index);
         var labelText = '';
+        var textPosition;
 
         if (tile.hasBuilding()) {
             labelText = tile.getBuilding().playerLabel;
+            textPosition = view.height/4;
+
+            // If the building is under construction
+            if (tile.getBuilding().startingTurn > MainGame.global.turn) {
+                view.underConst = MainGame.game.make.text(0, 0, 'Under Construction', this.fontBrief[1]);
+                view.underConst.x = view.width/2 - view.underConst.width/2
+                view.underConst.y = view.height/2;
+                view.addChild(view.underConst);
+            } else {
+                // Do some setup before we go
+                TileBriefView.setUpOccupancyDisplay(view, tile.getBuilding().people, tile.getBuilding().maxPeople);
+            }
         } else {
             labelText = tile.terrainLabel;
+            textPosition = view.height/2;
         }
 
-        view.label = MainGame.game.make.text(view.texture.width*0.5, view.texture.height*0.5, labelText, this.fontBrief[0]);
+        view.label = MainGame.game.make.text(view.width/2, textPosition, labelText, this.fontBrief[0]);
         view.label.anchor.set(0.5);
         view.addChild(view.label);
 
@@ -44,5 +53,27 @@ var TileBriefView = {
         view.x = board.x + tile.x*board.currentScale;
         view.y = board.y + (tile.y + tile.height*7)*board.currentScale;
         view.scale.set(board.currentScale);
-	}
+	},
+
+    setUpOccupancyDisplay: function(view, occupantCount, maxSize) {
+        var occupancyDisplay = MainGame.game.make.sprite(0, view.height/2);
+        var occupantWidth = MainGame.game.make.sprite(0, 0, 'person_icon').width;
+
+        for (var i = 0; i < maxSize; i++) {
+            occupant = MainGame.game.make.sprite(0, 0, 'person_icon');
+            occupant.x = occupant.width*0.5*i;
+
+            // Tint it to show occupancy
+            if (i < occupantCount) {
+                occupant.tint = 0x00ff00;
+            }
+
+            occupancyDisplay.addChild(occupant);
+        }
+
+        // Center the bar
+        occupancyDisplay.x = -occupantWidth/4 + (view.width/2) - ((occupantWidth*maxSize*0.5)/2);
+
+        view.addChild(occupancyDisplay);
+    },
 };
